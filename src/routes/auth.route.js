8b8a7e1d0b90ac34312959c6d46903afa77afb99
Trantien03 <115622082+Trantien03@users.noreply.github.com>
2023-11-06 21:04:32 +0700

    Nop bai thi

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,6 @@ const controller = require("../controller/auth.controller");
 
 router.get("/addProduct",controller.product);
 router.post("/addProduct",controller.postProduct);
-
+router.get("/product/delete/:id", controller.deleteProduct)
 
 module.exports = router;
