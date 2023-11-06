2819b6543c84a1f6c5f9063d43d5cadb645e63bd
Trantien03 <115622082+Trantien03@users.noreply.github.com>
2023-11-06 21:02:23 +0700

    Nop bai thi

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -8,15 +8,6 @@ exports.postProduct= function(req,res){
     const data = req.body;
 
        try {
-        if(req.file){
-            const file = req.file;
-            const fq = require("fs");
-            const img = fq.readFileSync(file.path);
-            data.thumbnail ={
-                contentType: file.mimetype,
-                data: img.toString("base64")
-            }
-        }
        const u = new productModel(data);
        u.save();
     
@@ -24,6 +15,20 @@ exports.postProduct= function(req,res){
        } catch (error) {
         return res.send(error)
     }
-
 }
- 
+exports.deleteProduct = async (req, res) => {
+    const productId = req.params.id;
+    console.log(productId);
+    try {
+      const deletedProduct = await productModel.findByIdAndDelete(productId);
+  
+      if (!deletedProduct) {
+        return res.send("Product not found");
+      }
+  
+      res.send("Product deleted successfully");
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+};
